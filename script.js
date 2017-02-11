63eebb50efc839ffa050fc607845c5c2bef5a322
allisonlu <allisonlu820@gmail.com>
2017-02-11 14:59:24 -0800

    button toggling

--- script.js
+++ script.js
@@ -2,21 +2,47 @@ $(document).ready(function(){
     $("#addButton").click(function(){
       $("#oneScalar").hide();
       $("#twoVectors").show();
+      $("#vAddW").show();
+      $("#vSubW").hide();
+      $("#scalMultResult").hide();
+      $("#vDotW").hide();
+      $("#crossProdResult").hide();
     });
-    // $("#subButton").click(function(){
-    //   $("#oneScalar").hide();
-    //   $("#twoVectors").show();
-    // });
-    // $("#scalarMult").click(function(){
-    //   $("#twoVectors").hide();
-    //   $("#oneScalar").show();
-    // });
+    $("#subButton").click(function(){
+      $("#oneScalar").hide();
+      $("#twoVectors").show();
+      $("#vAddW").hide();
+      $("#vSubW").show();
+      $("#scalMultResult").hide();
+      $("#vDotW").hide();
+      $("#crossProdResult").hide();
+    });
+    $("#scalarMult").click(function(){
+      $("#twoVectors").hide();
+      $("#oneScalar").show();
+      $("#vAddW").hide();
+      $("#vSubW").hide();
+      $("#scalMultResult").show();
+      $("#vDotW").hide();
+      $("#crossProdResult").hide();
+    });
+    // dot product doesn't work!
     // $("#dotProd").click(function(){
     //   $("#oneScalar").hide();
     //   $("#twoVectors").show();
+    //   $("#vAddW")hide();
+    //   $("#vSubW").hide();
+    //   $("#scalMultResult").hide();
+    //   $("#vDotW").show();
+    //   $("#crossProdResult").hide();
     // });
-    // $("#crossProd").click(function(){
-    //   $("#oneScalar").hide();
-    //   $("#twoVectors").show();
-    // });
+    $("#crossProd").click(function(){
+      $("#oneScalar").hide();
+      $("#twoVectors").show();
+      $("#vAddW").hide();
+      $("#vSubW").hide();
+      $("#scalMultResult").hide();
+      $("#vDotW").hide();
+      $("#crossProdResult").show();
+    });
 });
