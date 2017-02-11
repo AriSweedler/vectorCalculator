61e8d0c7497380acc01c34bf47aa167c46ce0019
Ari Sweedler <ari@sweedler.com>
2017-02-11 14:08:51 -0800

    Starting to create framework for vectors
    Can create a vector object with strings or numbers as input.
    
    Has an addition function that takes two vectors and returns a third
    vector that is the addition of the arguments

---
+++ vector.js
@@ -0,0 +1,34 @@
+var vector = {x: 0, y: 0, z: 0};
+
+function VectorConstruct(x, y, z){
+	this.x = toNum(x);
+	this.y = toNum(y);
+	this.z = toNum(z);
+
+	console.log("Your new vector is (" + x + ", " + y + ", " + z + ").");
+}
+
+function toNum(s){
+	var answer = 0;
+
+	if(typeof(s) == "number") {
+		answer = s;
+	}
+	else if (typeof(s) == "string") {
+		for(var i = 0; i < s.length; i++){
+			answer *= 10;
+			answer += s.charAt(i) - '0';
+		}
+	}
+	return answer;
+}
+
+
+function addVector(v1, v2){
+	var answer = VectorConstruct(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+	return answer;
+}
+
+var myVector1 = new VectorConstruct("1", "2", "3");
+var myVector2 = new VectorConstruct("10", "10", "10");
+var myVector3 = addVector(myVector1, myVector2);
