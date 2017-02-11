bdeabe98b210e15bb69dc1def9b0709d0a9275e0
allisonlu <allisonlu820@gmail.com>
2017-02-11 14:00:04 -0800

    jquery

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#addButton").click(function(){
+      $("#oneScalar").hide();
+      $("#twoVectors").show();
+    });
+    // $("#subButton").click(function(){
+    //   $("#oneScalar").hide();
+    //   $("#twoVectors").show();
+    // });
+    // $("#scalarMult").click(function(){
+    //   $("#twoVectors").hide();
+    //   $("#oneScalar").show();
+    // });
+    // $("#dotProd").click(function(){
+    //   $("#oneScalar").hide();
+    //   $("#twoVectors").show();
+    // });
+    // $("#crossProd").click(function(){
+    //   $("#oneScalar").hide();
+    //   $("#twoVectors").show();
+    // });
+});
