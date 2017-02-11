b1a5dae225cda2643464e060b58bf9e2f068350f
Ari Sweedler <ari@sweedler.com>
2017-02-11 15:14:42 -0800

    Added cross product
    In addition, return null if lengths of vectors are not equal when
    adding/subtracting.
    
    Cross product only accepts two 3D variables

--- vector.js
+++ vector.js
@@ -3,7 +3,7 @@ function Vector(x, y, z){
 	this.y = toNum(y);
 	this.z = toNum(z);
 
-	console.log("Your new vector is (" + x + ", " + y + ", " + z + ").");
+	//console.log("Your new vector is (" + x + ", " + y + ", " + z + ").");
 }
 
 function toNum(s){
@@ -13,6 +13,11 @@ function toNum(s){
 		answer = s;
 	}
 	else if (typeof(s) == "string") {
+
+		if (s == ""){
+			return null;
+		}
+
 		for(var i = 0; i < s.length; i++){
 			answer *= 10;
 			answer += s.charAt(i) - '0';
@@ -23,6 +28,10 @@ function toNum(s){
 
 
 function addVector(v1, v2){
+	if (vectorLength(v1) != vectorLength(v2)){
+		return;
+	}
+
 	var answer = v1;
 	for(var n in v1){
 		answer[n] = v1[n] + v2[n];
@@ -45,9 +54,43 @@ function subtractVector(v1, v2){
 }
 
 function dotProduct(v1, v2){
+	if (vectorLength(v1) != vectorLength(v2)){
+		return;
+	}
+
 	var answer = 0;
 	for(var n in v1){
 		answer += v1[n]*v2[n];
 	}
+	return answer;
+}
+
+function vectorLength(v){
+	var count = 0;
+	for(n in v){
+		if (v[n] != null){
+			count++;
+		}
+	}
+	return count;
+}
+
+function crossProduct(v1, v2){
+	if (vectorLength(v1) != 3 || vectorLength(v2) != 3){
+		console.log("Lengths are not both 3, terminating.");
+		return;
+	}
+
+	var answer = new Vector(0, 0, 0);
+	answer.x = (v1.y*v2.z - v2.y*v1.z);
+	answer.y = (v1.z*v2.x - v2.z*v1.x) * -1;
+	answer.z = (v1.x*v2.y - v2.x*v1.y);
+
+	for(var n in answer){
+		if (answer[n] === 0){
+			answer[n] = 0;
+		}
+	}	// prevents answers from equalling -0
+
 	return answer;
 }
